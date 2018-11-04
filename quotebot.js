fcd28fbfe64d365830573b4a2a013ac12b716035
Will <slithid@gmail.com>
2018-11-04 16:50:11 -0500

    quote update

--- quotebot.js
+++ quotebot.js
@@ -1,15 +1,26 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
- 
-client.on("ready", () => {
-  console.log("I am ready!");
-});
- 
-client.on("message", (message) => {
-  console.log(message.content);
-  if (message.content.includes("ping")) {
-    message.channel.send("pong!");
-  }
-});
- 
+const shell = require("shelljs");
+
+function ready() {
+	if (!shell.which('fortune')) {
+        shell.echo('Sorry, this script requires fortune');
+        shell.exit(1);
+    } else {
+        console.log("I am ready!");
+    }
+}
+
+function quote(message) {
+    console.log(message.content);
+    if (message.content.startsWith("!quote")) {
+        let quote = shell.exec("fortune").stdout;
+        message.channel.send(quote);
+    }
+}
+
+client.on("ready", ready);
+
+client.on("message", quote);
+
 client.login(process.env.DISCORD_TOKEN);
